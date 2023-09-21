4a8eb69ebfbad9ffdd52b2a33a39f99821d7e5ad
Tyuiu.TolstykhViacheslav@outlook.com <Tyuiu.TolstykhViacheslav@outlook.com>
2023-09-21 13:15:30 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.TolstykhVS.Sprint0.Task1.Vo/Tyuiu.TolstykhVS.Sprint0.Task1.Vo.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namecpace std;
+
+int main()
+{   
+     cout << "Hello World!\n";
+    cout << "Введите свое ФИО : ";
+    string a;
+    cin >> a;
+    int b;
+    cin >> b;
+    cout << "Возраст = " << b;
+    return 0;
+}
---
+++ Tyuiu.TolstykhVS.Sprint0.Task1.cpp/Tyuiu.TolstykhVS.Sprint0.Task1.cpp.cpp
@@ -0,0 +1,17 @@
+﻿// Tyuiu.TolstykhVS.Sprint0.Task1.cpp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+     setlocale(LC_ALL, "Russian");
+     cout << "Hello World!\n";
+     cout << "Введите свое ФИО : ";
+     string a;
+     cin >> a;
+     int b;
+     cin >> b;
+     cout << "Возраст = " << b;
+     return 0;
+    }
